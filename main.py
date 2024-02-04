aa3bf7c757ac2d52c1bd9eaf7bf6f2af1438c3d6
r0th3r <503407184@qq.com>
2024-02-04 20:10:23 +0800

    update

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
 
     from aobscan import program_aobscan
 
+    import os, sys
+    updater = program_aobscan(
+        sys.argv[1], 
+        os.path.join(os.path.dirname(sys.argv[0]), "aobscan" ,"aobscan2.js"))
 
-    app = "notepad.exe" # or pid
-
-    updater = program_aobscan(app, "aobscan\\aobscan2.js")
-
-    updater.scan("update_example.json"); updater.export("export.json")
+    updater.scan(sys.argv[2]); updater.export(sys.argv[3])
 
     print("done")
